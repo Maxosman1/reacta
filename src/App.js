9cb063be0688b45bee7762e5e15e277d754c680a
Maxosman1 <120883114+Maxosman1@users.noreply.github.com>
2023-11-22 13:20:00 -0600

    1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Navigation } from './common/navigation';
+import { Poppins_300Light ,Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold, Poppins_700Bold } from '@expo-google-fonts/poppins';
+import { useFonts } from 'expo-font';
+
+const App = () => {
+  
+  const [fontsLoaded, fontError] = useFonts({ Poppins_300Light, Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold, Poppins_700Bold });
+
+  if (!fontsLoaded && !fontError) return null;
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Navigation />
+    </SafeAreaView>
+  );
+};
+
+export default App;
